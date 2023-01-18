7ce204788d6eedd42d2eea12a1279e5a8ddeab44
Shahin-Alz <shaalemzadeh@gmail.com>
2023-01-18 14:40:14 +0100

    finish

---
+++ main.js
@@ -0,0 +1,83 @@
+// let recup = document.getElementsByTagName("div")[0];
+// console.log(recup);
+// let firstH3 = recup.firstElementChild;
+// console.log(firstH3.textContent);
+
+///////////////////
+
+// let titre = document.getElementsByTagName("h3")[1];
+// console.log(titre);
+
+////////////////////
+
+// let titre = document.getElementsByTagName("h3")[1];
+// let avant = titre.previousElementSibling;
+// console.log(avant.textContent);
+
+///////////////
+
+// let para = document.getElementsByTagName("p")[1]
+// let apres = para.nextElementSibling;
+// console.log(apres);
+
+////////////////
+
+// let liste = document.getElementsByTagName("ul")[0];
+// console.log(liste.textContent);
+// let parent = liste.parentNode;
+// console.log(parent.textContent);
+// let first = parent.firstChild;
+// console.log(first.textContent);
+
+///////////////
+
+// let first = document.getElementsByClassName("important")[3];
+// let apres = first.nextElementSibling;
+// console.log(apres.textContent);
+
+//////////////
+
+// let prems = document.getElementById("textGeneral");
+// console.log(prems);
+// let dos = prems.nextElementSibling; 
+// let tres = dos.nextElementSibling;
+// console.log(tres.textContent);
+
+///////////////
+
+// let span1 = document.getElementsByTagName("span")[1]
+// console.log(span1.textContent);
+// let dos = span1.parentNode;
+// console.log(dos);
+// let tres = dos.nextSibling;
+// console.log(tres);
+
+///////////////
+
+// let nicolas = document.getElementsByTagName("b")[2];
+// console.log(nicolas.textContent);
+// let avant = nicolas.parentElement;
+// console.log(avant.textContent);
+// let avantavant = avant.parentElement;
+// console.log(avantavant.textContent);
+// let para = avantavant.previousElementSibling;
+// console.log(para.textContent);
+// let enfant = para.firstElementChild;
+// console.log(enfant.textContent);
+// let petitenfant = enfant.firstElementChild;
+// console.log(petitenfant.textContent);
+
+///////////////
+
+// let GrandPara = document.querySelector(".grandParagraphe");
+// console.log(GrandPara.textContent);
+
+//////////////
+
+// let recup = document.querySelectorAll("li");
+// for (let index = 0; index < recup.length; index++) {
+//     console.log(recup[index].textContent);
+    
+// }
+
+//////////////
